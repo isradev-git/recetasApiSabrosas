afed91b471bd016a06278a122cdd1286cbedaa06
isradev-git <israelzamora1996@gmail.com>
2024-02-19 11:59:18 +0100

    Cambios en funcionamiento app

--- js/app.js
+++ js/app.js
@@ -62,6 +62,19 @@ function mealRecipeModal(meal) {
         <h2 class="recipe-title">${meal.strMeal}</h2>
         <p class="recipe-category">${meal.strCategory}</p>
         <p class="recipe-category">${meal.strArea}</p>
+        <p class="recipe-category">${meal.strTags}</p>
+        <div class="recipe-ingredients">
+            <h3>Ingredients</h3>
+            <p>${meal.strIngredient1 +" -- "+ meal.strMeasure1}</p>
+            <p>${meal.strIngredient2 +" -- "+ meal.strMeasure2}</p>
+            <p>${meal.strIngredient3 +" -- "+ meal.strMeasure3}</p>
+            <p>${meal.strIngredient4 +" -- "+ meal.strMeasure4}</p>
+            <p>${meal.strIngredient5 +" -- "+ meal.strMeasure5}</p>
+            <p>${meal.strIngredient6 +" -- "+ meal.strMeasure6}</p>
+            <p>${meal.strIngredient7 +" -- "+ meal.strMeasure7}</p>
+            <p>${meal.strIngredient8 +" -- "+ meal.strMeasure8}</p>
+            <p>${meal.strIngredient9 +" -- "+ meal.strMeasure9}</p>
+        </div>
         <div class="recipe-instruct">
             <h3>Intructions</h3>
             <p>${meal.strInstructions}</p>
